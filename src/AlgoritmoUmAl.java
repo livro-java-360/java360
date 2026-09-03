4f974fc7fa8098fbaab4f2e1bc2fce1488b79284
= <=>
2026-09-03 10:41:21 -0300

    fiz o algoritmo um do aluno

--- src/AlgoritmoUmAl.java
+++ src/AlgoritmoUmAl.java
@@ -1,3 +1,9 @@
 public class AlgoritmoUmAl {
+
+    void main(){
+        IO.println("Quem ousa entrar na sala do Eco");
+        String nome = IO.readln("Qual o seu nome?");
+        IO.println("Ah..."+nome+". Eu esperava por você.");
+    }
     
 }
